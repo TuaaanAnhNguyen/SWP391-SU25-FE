b4bb5a7d745acf91a06bdb40b8416c463fd19e2e
TuaaanAnhNguyen <ntuananh2503@gmail.com>
2025-06-10 02:08:26 +0700

    created header component

--- smoking-cessation/src/app/app.routes.ts
+++ smoking-cessation/src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Login } from './login/login';
-import { Signup } from './signup/signup';
-import { Homepage } from './homepage/homepage';
 
 export const routes: Routes = [
-    { path: 'login', component: Login},
-    { path: 'signup', component: Signup},
-    { path: 'homepage', component: Homepage },
-    {path: 'blog', loadComponent: () => import('./blog/blog').then(m => m.Blog)},
+    { path: 'login', loadComponent: () => import('./login/login').then(m => m.Login) },
+    { path: 'signup', loadComponent: () => import('./signup/signup').then(m => m.Signup) },
+    { path: 'homepage', loadComponent: () => import('./homepage/homepage').then(m => m.Homepage) },
+    { path: 'blog', loadComponent: () => import('./blog/blog').then(m => m.Blog) },
     { path: '', redirectTo: '/homepage', pathMatch: 'full' },
 ];
 
--- smoking-cessation/src/app/app.ts
+++ smoking-cessation/src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'smoking-cessation';
+
+  private router = inject(Router);
+
+  constructor() {}
+
+  showHeader() {
+    return !['/login', '/signup'].includes(this.router.url);
+  }
 }
