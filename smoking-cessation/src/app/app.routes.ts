c950f8b460083b954a9d9a3f44450282eae941ad
TuaaanAnhNguyen <ntuananh2503@gmail.com>
2025-06-15 19:18:35 +0700

    temporarily merge profile-page

--- smoking-cessation/src/app/app.routes.ts
+++ smoking-cessation/src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
     { path: 'signup', loadComponent: () => import('./signup/signup').then(m => m.Signup) },
     { path: 'homepage', loadComponent: () => import('./homepage/homepage').then(m => m.Homepage) },
     { path: 'blog', loadComponent: () => import('./blog/blog').then(m => m.Blog) },
+    { path: 'profilepage', loadComponent: () => import('./profilepage/profilepage').then(m => m.Profilepage) },
     { path: 'admin-dashboard', loadComponent: () => import('./admin-dashboard/admin-dashboard').then(m => m.AdminDashboard),
         children: [
             {
---
+++ smoking-cessation/src/app/profilepage/profilepage.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profilepage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profilepage.html',
+  styleUrl: './profilepage.css'
+})
+export class Profilepage {
+
+}
