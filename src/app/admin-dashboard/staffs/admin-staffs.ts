941ce4cebc79b3b773f4d1b2071c062a03779c96
Nhahnt <trongnha140204@gmail.com>
2025-06-20 01:17:41 +0700

    app

---
+++ src/app/admin-dashboard/staffs/admin-staffs.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-staffs',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './admin-staffs.html',
+  styleUrls: ['./admin-staffs.css'],
+})
+
+export class AdminStaffs {
+
+}    
